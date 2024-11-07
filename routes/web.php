200199d44014bb522f0255d4ad8426e2c52a2892
Luis Henrique <junimhs1996@gmail.com>
2024-11-07 13:07:28 -0300

    chore: initial commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
