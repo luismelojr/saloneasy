c15af126a34d1519c5101581295b9421d2c7cf6b
Luis Henrique <junimhs1996@gmail.com>
2024-11-26 15:38:54 -0300

    feat: adicionando no select

--- resources/js/Pages/Dashboard/ScheduleManually/Screens/Index.tsx
+++ resources/js/Pages/Dashboard/ScheduleManually/Screens/Index.tsx
@@ -2,12 +2,15 @@ import DashboardLayout from '@/components/layouts/dashboard-layout';
 import CardShared from '@/components/shared/Card';
 import CardContentShared from '@/components/shared/CardContentShared';
 import CardTitleShared from '@/components/shared/CardTitleShared';
+import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
+import { SelectCustom } from '@/components/ui/select-custom';
 import { SelectAsyncCustom } from '@/components/ui/select-custom-async';
 import { ClientShortInterface } from '@/types';
 import { ScheduleManuallyFormInterface } from '@/types/forms';
 import { useForm } from '@inertiajs/react';
 import axios from 'axios';
+import { Plus } from 'lucide-react';
 
 const menus = [
     {
@@ -47,6 +50,11 @@ export default function Index({ services }: ScheduleManuallyProps) {
         }
     };
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(form.data);
+    };
+
     return (
         <DashboardLayout menus={menus}>
             <CardShared>
@@ -57,24 +65,36 @@ export default function Index({ services }: ScheduleManuallyProps) {
                     }
                 />
                 <CardContentShared>
-                    <div className={'space-y-6'}>
+                    <form onSubmit={handleSubmit} className={'space-y-6'}>
                         <div className={'grid w-full items-center gap-2'}>
                             <Label>Cliente</Label>
                             <SelectAsyncCustom
                                 createAble={false}
                                 isMulti={false}
                                 loadOptions={loadOptions}
+                                onChange={(item) => {
+                                    form.setData('client_id', item.value);
+                                }}
                             />
                         </div>
                         <div className={'grid w-full items-center gap-2'}>
                             <Label>Serviço</Label>
-                            <SelectAsyncCustom
+                            <SelectCustom
                                 options={services}
                                 createAble={false}
                                 isMulti={false}
+                                onChange={(item) => {
+                                    form.setData('service_id', item.value);
+                                }}
                             />
                         </div>
-                    </div>
+                        <div className={'flex w-full justify-end'}>
+                            <Button type="submit" loading={form.processing}>
+                                <Plus className="mr-2 h-4 w-4" />
+                                Salvar
+                            </Button>
+                        </div>
+                    </form>
                 </CardContentShared>
             </CardShared>
         </DashboardLayout>
